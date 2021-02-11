87891a51cd235a48250fa3adbab5898ce748ebb8
Hugo Mozo <hmozo@outlook.com>
2021-02-11 11:17:49 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import tasksReducer from './reducers'
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from './middleware/logger';
 
 
 const rootReducer= (state={}, action)=>{
@@ -21,7 +20,7 @@ const rootReducer= (state={}, action)=>{
 
 const store= createStore(
   rootReducer, 
-  composeWithDevTools(applyMiddleware(thunk, logger))
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 /*
