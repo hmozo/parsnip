e8ee3e95474843482ed4f6af192ea95f8ede6ede
Hugo Mozo <hmozo@outlook.com>
2021-02-11 09:51:00 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
 import TasksPage from './components/TasksPage'
+import { connect } from 'react-redux'
 
 import './App.css';
 
-const mockTasks= [
-  {
-    id: 1,
-    title: 'Learn Redux',
-    description: 'The store, actions, and reducers, oh my!',
-    status: 'In Progress',
-  },
-  {
-    id: 2,
-    title: 'Peace on Earth',
-    description: 'No big deal.',
-    status: 'In Progress',
-  },
-];
+import { createTask, editTask } from './actions';
+import { fetchTasks } from './actions'
+import FlashMessage from './components/FlashMessage';
+
 
 class App extends Component{
+
+  componentDidMount(){
+    this.props.dispatch(fetchTasks());
+    //this.props.subscribe(()=>console.log('hola'));
+    this.forceUpdate();
+  }
+
+  onCreateTask= ({ title, description })=>{
+    this.props.dispatch(createTask({ title, description }));
+    this.forceUpdate()
+  }
+
+  onStatusChange= (id, status)=>{
+    this.props.dispatch(editTask(id, { status }));
+    this.forceUpdate();
+  }
+
   render(){
+    console.log('props from App: ', this.props);
     return(
-      <div className='main-content'>
-        <TasksPage tasks={mockTasks} />
+      <div className='container'>
+        {this.props.error &&
+          <FlashMessage message= {this.props.error} />
+        }
+        <TasksPage 
+          tasks={this.props.tasks} 
+          onCreateTask={this.onCreateTask}  
+          onStatusChange={this.onStatusChange}
+          isLoading={this.props.isLoading}
+        />
       </div>
     )
   }
 }
 
+function mapStateToProps(state){
+  const { tasks, isLoading, error }= state.tasks;
+  return { tasks, isLoading, error }
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import tasksReducer from './reducers'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+const rootReducer= (state={}, action)=>{
+  return {
+    tasks: tasksReducer(state.tasks, action),
+  };
+};
+
+const store= createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(thunk))
 );
+
+/*
+if (module.hot){
+
+  module.hot.accept('./reducers', ()=>{
+    const nextRootReducer= require('./reducers').default;
+    store.replaceReducer(nextRootReducer);
+  });
+
+  module.hot.accept('./App', ()=>{
+    const NextApp= require('./App').default;
+    ReactDOM.render(
+      <Provider store={store}>
+        <NextApp />
+      </Provider>,
+      document.getElementById('root')
+    );
+  });
+
+  
+ 
+}else{
+  */
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+//}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
